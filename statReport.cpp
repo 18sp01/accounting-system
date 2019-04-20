2fb01f9db7b8e05d03c4e40d36c4c2354cd9f4e6
18sp01 <18sp01@gmail.com>
2019-04-20 16:15:10 +0800

    Added interface for incomeReports and expenseReports

--- statReport.cpp
+++ statReport.cpp
@@ -7,22 +7,49 @@
 using namespace std;
 
 void listReports(int numRow, int numCol, string *&reports, int sizeReports) {
-    cout << setw(5) << left << '#' << " | ";
+    /*cout << setw(5) << left << '#' << " | ";
     cout << setw(15) << left << "Type";
     cout << endl;
     for (int i = 0; i < sizeReports; i++) {
         cout << setw(5) << left << i+1 << " | ";
         cout << setw(15) << reports[i];
         cout << endl;
+    }*/
+    for (int i = 0; i < sizeReports; i++) {
+        cout << "[" << i+1 << "] ";
+        cout << reports[i];
+        cout << endl;
     }
 }
 
-void incomeReports() {
-    cout << "income reports" << endl;
+int incomeReports(int numRow, int numCol, record *&records, int &sizeArray) {
+    printTopRow(numCol);
+    cout << "Income Reports" << endl;
+    cout << "Please enter a number to view the corresponding report, or enter [x] to exit" << endl;
+    cout << endl;
+    for (int i = 0; i < numRow - 7; i++)
+        cout << endl;
+    cout << "[x] Exit" << endl;
+    printBottomRow(numCol);
+    char input;
+    cin >> input;
+    if (input == 'x')
+        return 0;
 }
 
-void expenseReports() {
-    cout << "expense reports" << endl;
+int expenseReports(int numRow, int numCol, record *&records, int &sizeArray) {
+    printTopRow(numCol);
+    cout << "Expense Reports" << endl;
+    cout << "Please enter a number to view the corresponding report, or enter [x] to exit" << endl;
+    cout << endl;
+    for (int i = 0; i < numRow - 7; i++)
+        cout << endl;
+    cout << "[x] Exit" << endl;
+    printBottomRow(numCol);
+    char input;
+    cin >> input;
+    if (input == 'x')
+        return 0;
 }
 
 void statReports(int numRow, int numCol, record *&records, int &sizeArray) {
@@ -35,7 +62,7 @@ void statReports(int numRow, int numCol, record *&records, int &sizeArray) {
         cout << "Please enter a number to view the corresponding report, or enter [x] to exit" << endl;
         cout << endl;
         listReports(numRow, numCol, reports, sizeReports);
-        for (int i = 0; i < numRow - 11; i++)
+        for (int i = 0; i < numRow - 7 - sizeReports; i++)
             cout << endl;
         cout << "[x] Exit" << endl;
         printBottomRow(numCol);
@@ -43,18 +70,15 @@ void statReports(int numRow, int numCol, record *&records, int &sizeArray) {
         if (input == 'x')
             break;
         if (input == '1') {
-            incomeReports();
-            break;
+            incomeReports(numRow, numCol, records, sizeArray);
         }
         // filter by account
         if (input == '2') {
-            expenseReports();
-            break;
+            expenseReports(numRow, numCol, records, sizeArray);
         }
         // filter by date
         if (input == '3') {
             //other;
-            break;
         }
     }
 }
